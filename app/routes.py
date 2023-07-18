e67ca265f2ffc55188c3153982aed4c1229864c5
viraj28 <vnirbhavane28@gmail.com>
2023-07-18 12:19:21 +0000

    add templates

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,19 @@
+from flask import render_template
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-  user = {'username': 'Miguel'}
-  return '''
-    <html>
-        <head>
-            <title>Home Page - Microblog</title>
-        </head>
-        <body>
-            <h1>Hello, ''' + user['username'] + '''!</h1>
-        </body>
-    </html>'''
+  user = {'username': 'Viraj'}
+  posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+  return render_template("index.html", title="", user=user, posts=posts)
