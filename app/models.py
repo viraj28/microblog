383b4e708dc08cfee7b5b5f42666484869aa7654
viraj28 <vnirbhavane28@gmail.com>
2023-08-08 05:50:13 +0000

    add followed_posts()

--- app/models.py
+++ app/models.py
@@ -28,11 +28,36 @@ class User(UserMixin, db.Model):
   def check_password(self, password):
     return check_password_hash(self.password_hash, password)
 
+  
   def avatar(self, size):
     digest = md5(self.email.lower().encode('utf-8')).hexdigest()
     return 'https://gravatar.com/avatar/{}?d=identicon&s={}'.format(
       digest, size)
 
+  def follow(self, user):
+    if not self.is_followig(user):
+      self.followed.append(user)
+
+  def unfollow(self, user):
+    if self.is_followig(user):
+      self.followed.remove(user)
+
+  def is_following(self, user):
+    return self.followed.filter(followed.c.followed_id == user.id).count() > 0
+  
+
+  def followed_posts(self):
+    followed = Post.query.join(
+      followers, (followers.c.followed_id == Post.user_id)
+    ).filter(
+      followers.c.follower_id == self.id
+    )
+
+    own = Post.query.filter_by(user_id == self.id)
+
+    return followed.union(own).order_by(Post.timestamp.desc())
+
+  
   def __repr__(self):
     return '<User {}>'.format(self.username)
 
